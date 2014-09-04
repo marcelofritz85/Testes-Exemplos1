f3855cb84e78385b4e80b41d7e05504af50dc505
Marcelo <marceloyo85@gmail.com>
2014-09-04 11:16:58 -0300

    Metodo procura elemento em um arquivo e mostra

---
+++ ProcurarElementoArquivo/src/Main.java
@@ -0,0 +1,16 @@
+import Metodos.Metodo;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Metodo roda = new Metodo();
+		
+		roda.procura();
+	}
+
+}
